cf1760b5e5217f0f59a5c0bc57d1afcb54570a3f
codelrond78 <90870337+codelrond78@users.noreply.github.com>
2022-05-28 19:28:11 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Counter from '$lib/components/Counter.svelte'
+	import Go from '$lib/components/Go.svelte'
+</script>
+
+<button class="btn btn-warning">Hello ;)</button>
+
+<Counter /><br/>
+
+<Go /><br />
+
+<a href="/about">About</a><br />
