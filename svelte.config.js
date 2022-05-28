79177cf5e917854aa8f414c4b602f1b909776e4a
codelrond <codelrond@protonmail.com>
2022-05-28 20:05:05 +0200

    added md support and layout

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,22 @@ import preprocess from 'svelte-preprocess';
 import { VitePWA } from 'vite-plugin-pwa';
 import replace from '@rollup/plugin-replace'
 import { pwaConfiguration, replaceOptions } from './pwa-configuration.js'
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: [
+		preprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: {
+				blog: 'src/routes/blog/_post.svelte'
+			}
+		})
+	],
+	extensions: ['.svelte', '.md'],
 	kit: {
 		adapter: adapter(),
 		vite: {
